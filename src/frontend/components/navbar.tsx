cc3b02fdca5e9df43302c638f7f0e94a4c4542d7
lautarobleiz <lautaro.bleiz77@gmail.com>
2026-01-27 16:04:21 -0300

    No se que verga de colores elegir

--- src/frontend/components/navbar.tsx
+++ src/frontend/components/navbar.tsx
@@ -4,14 +4,14 @@ import '../styles/navbar.css'
 export function Navbar (){
     return (
         <>
-            <div className='navbar-main'>
+            <div className='navbar-main shadow-lg/30'>
                 <div className='navbar-slogan'>
-                    <h1 className="text-4xl text-yellow-400 font-semibold">Nexo Store</h1>
-                    <p className='navbar-store-description'>La mejor electrónica, al mejor precio.</p>
+                    <p className="text-4xl text-gray-300 font-semibold text-shadow-lg/20">Nexo Store</p>
+                    <p className='navbar-store-description font-semibold text-white text-shadow-lg/20'>La mejor electrónica, al mejor precio.</p>
                 </div>
                 <div className="navbar-buttons-container">
-                    <Button className="bg-yellow-500 hover:bg-yellow-600">Iniciar sesión</Button>
-                    <Button className="bg-yellow-500 hover:bg-yellow-600">Registrarse</Button>
+                    <Button className="bg-yellow-300 hover:bg-yellow-400 text-gray-600 border border-gray-700">Iniciar sesión</Button>
+                    <Button className="bg-yellow-300 hover:bg-yellow-400 text-gray-600 border border-gray-600">Registrarse</Button>
                 </div>
             </div>    
         </>
