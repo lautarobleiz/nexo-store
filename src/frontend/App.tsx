7eaa37c14990da9e0e0c2c708822cbe2e601db44
Lautaro Francisco Bleiz <lautaro.bleiz77@gmail.com>
2026-01-28 11:56:16 -0300

    Estructura de los elementos en venta creada

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -7,8 +7,45 @@ function App() {
       <div className="app-main-container">    
         <Navbar />
         <div className="app-main-grid-container">
-          <div className="app-main-items-grid bg-yellow-200">
+          <div className="grid grid-cols-3 gap-4 w-full p-4">
+            {/* Elemento 1 */}
+            <div className="bg-slate-100 p-6 rounded-lg shadow-sm border">
+              <h3 className="font-bold">Resistencia 10k</h3>
+              <p className="text-sm text-gray-500">$0.50</p>
+            </div>
             
+            {/* Elemento 2 */}
+            <div className="bg-slate-100 p-6 rounded-lg shadow-sm border">
+
+              <h3 className="font-bold">Arduino Uno R3</h3>
+              <p className="text-sm text-gray-500">$25.00</p>
+            </div>
+
+            {/* Elemento 3 */}
+            <div className="bg-slate-100 p-6 rounded-lg shadow-sm border">
+              <h3 className="font-bold">Sensor Ultrasonido</h3>
+              <p className="text-sm text-gray-500">$3.20</p>
+            </div>
+
+            {/* Elemento 3 */}
+            <div className="bg-slate-100 p-6 rounded-lg shadow-sm border">
+              <h3 className="font-bold">Sensor Ultrasonido</h3>
+              <p className="text-sm text-gray-500">$3.20</p>
+            </div>
+
+            {/* Elemento 3 */}
+            <div className="bg-slate-100 p-6 rounded-lg shadow-sm border">
+              <h3 className="font-bold">Sensor Ultrasonido</h3>
+              <p className="text-sm text-gray-500">$3.20</p>
+            </div>
+
+            {/* Elemento 3 */}
+            <div className="bg-slate-100 p-6 rounded-lg shadow-sm border">
+              <h3 className="font-bold">Sensor Ultrasonido</h3>
+              <p className="text-sm text-gray-500">$3.20</p>
+            </div>
+
+            {/* La siguiente fila se crea automáticamente */}
           </div>
         </div>
       </div>
